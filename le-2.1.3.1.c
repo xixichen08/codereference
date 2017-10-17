cc517edab1a8885b31da029acde731eb267a17bd
xixichen08 <xixichen08@foxmail.com>
2017-10-17 12:07:42 +0800

    codereference-17.10.16

---
+++ le-2.1.3.1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int search(int *buffer,int n,int target){
+	int right=n;
+	int left=0;
+	while(left!=right){
+	     int mid=(right+left)/2;
+		if(buffer[mid]==target){
+			return mid;
+		}
+		if(buffer[left]<=buffer[mid]){
+			if(buffer[mid]>target&&buffer[left]<=target){
+				right=mid;
+			}
+			else
+				left=mid;
+		}
+		if(buffer[right-1]>=buffer[mid]){
+			if(buffer[mid]<target&&buffer[right-1]>=target){
+				left=mid;
+			}
+			else
+				right=mid;
+			
+		}		
+	}
+	return (right+left)/2;
+
+}
+int main (int argc,char **argv){
+	int A[11]={0,1,2,3,4,5,6,7,8,9,10};
+	int B[9]={5,6,7,8,0,1,2,3,4};
+	int C[7]={3,4,5,6,0,1,2};
+	printf("Index is %d\n",search(B,9,4));
+	printf("Index is %d\n",search(C,7,3));
+	
+	return 0;
+
+
+}
